ca2247b36d2048255d1c83895298641e5183132f
sacrac <crocha09.09@gmail.com>
2014-02-13 16:32:14 -0600

    fix orden noticias

--- notas/views.py
+++ notas/views.py
@@ -25,7 +25,7 @@ def logout_page(request):
   return HttpResponseRedirect('/')
 
 def lista_notas(request):
-    notas_list = Notas.objects.all().order_by('-fecha','-id')
+    notas_list = Notas.objects.all().order_by('id')
     paises = Pais.objects.all()
 
     paginator = Paginator(notas_list, 4)
